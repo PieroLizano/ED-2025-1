ecf1d7480d56f876febf9f417cf8c39efa448122
Piero <pierodariolizano@gmail.com>
2025-04-03 13:38:25 -0500

    lista simple

---
+++ Ejercicios/Form1.Designer.cs
@@ -0,0 +1,127 @@
+﻿namespace Ejercicios
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.sistemaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.operacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaEnlazadaSimpleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaEnlazadaCircularToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.listaEnlazadaDobleToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.sALIRToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Font = new System.Drawing.Font("Segoe UI", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sistemaToolStripMenuItem,
+            this.operacionesToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(598, 33);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // sistemaToolStripMenuItem
+            // 
+            this.sistemaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.sALIRToolStripMenuItem});
+            this.sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
+            this.sistemaToolStripMenuItem.Size = new System.Drawing.Size(92, 29);
+            this.sistemaToolStripMenuItem.Text = "Sistema";
+            // 
+            // operacionesToolStripMenuItem
+            // 
+            this.operacionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.listaEnlazadaSimpleToolStripMenuItem,
+            this.listaEnlazadaCircularToolStripMenuItem,
+            this.listaEnlazadaDobleToolStripMenuItem});
+            this.operacionesToolStripMenuItem.Name = "operacionesToolStripMenuItem";
+            this.operacionesToolStripMenuItem.Size = new System.Drawing.Size(134, 29);
+            this.operacionesToolStripMenuItem.Text = "Operaciones";
+            // 
+            // listaEnlazadaSimpleToolStripMenuItem
+            // 
+            this.listaEnlazadaSimpleToolStripMenuItem.Name = "listaEnlazadaSimpleToolStripMenuItem";
+            this.listaEnlazadaSimpleToolStripMenuItem.Size = new System.Drawing.Size(278, 30);
+            this.listaEnlazadaSimpleToolStripMenuItem.Text = "Lista enlazada simple";
+            this.listaEnlazadaSimpleToolStripMenuItem.Click += new System.EventHandler(this.listaEnlazadaSimpleToolStripMenuItem_Click);
+            // 
+            // listaEnlazadaCircularToolStripMenuItem
+            // 
+            this.listaEnlazadaCircularToolStripMenuItem.Name = "listaEnlazadaCircularToolStripMenuItem";
+            this.listaEnlazadaCircularToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.listaEnlazadaCircularToolStripMenuItem.Text = "Lista enlazada circular";
+            // 
+            // listaEnlazadaDobleToolStripMenuItem
+            // 
+            this.listaEnlazadaDobleToolStripMenuItem.Name = "listaEnlazadaDobleToolStripMenuItem";
+            this.listaEnlazadaDobleToolStripMenuItem.Size = new System.Drawing.Size(189, 22);
+            this.listaEnlazadaDobleToolStripMenuItem.Text = "Lista enlazada doble";
+            // 
+            // sALIRToolStripMenuItem
+            // 
+            this.sALIRToolStripMenuItem.Name = "sALIRToolStripMenuItem";
+            this.sALIRToolStripMenuItem.Size = new System.Drawing.Size(180, 30);
+            this.sALIRToolStripMenuItem.Text = "Salir";
+            this.sALIRToolStripMenuItem.Click += new System.EventHandler(this.sALIRToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(598, 289);
+            this.ControlBox = false;
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Estructura de Datos";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem sistemaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem operacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaEnlazadaSimpleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaEnlazadaCircularToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem listaEnlazadaDobleToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem sALIRToolStripMenuItem;
+    }
+}
+
---
+++ Ejercicios/FrmListaSimple.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicios
+{
+    public partial class FrmListaSimple: Form
+    {
+        clases.ListaSimple ls = new clases.ListaSimple();
+        public FrmListaSimple()
+        {
+            InitializeComponent();
+        }
+
+        private void txtNumero_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnInsertar_Click(object sender, EventArgs e)
+        {
+            listNumeros.Items.Clear();
+            ls.insertar(int.Parse(txtNumero.Text));
+            ls.mostrar(listNumeros);
+            txtNumero.Clear();
+            txtNumero.Focus();
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+            ls.eliminar(int.Parse(txtNumero.Text));
+            listNumeros.Items.Clear();
+            ls.mostrar(listNumeros);
+            txtNumero.Clear();
+            txtNumero.Focus();
+        }
+
+        private void btnOrdenar_Click(object sender, EventArgs e)
+        {
+            ls.ordenar();
+            listNumeros.Items.Clear();
+            ls.mostrar(listNumeros);
+        }
+
+        private void btnBuscar_Click(object sender, EventArgs e)
+        {
+            int num = int.Parse(txtNumero.Text);
+            
+            if (ls.buscar(num) != false) MessageBox.Show($"El número {num} existe en la lista");
+            else MessageBox.Show($"El número {num} no existe en la lista");
+        }
+    }
+}
